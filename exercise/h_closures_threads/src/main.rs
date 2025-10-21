7eda1bd4608354aaa64961caba558fada664571d
shfernan26 <sachin.fernando@flyzipline.com>
2025-10-21 18:46:29 +0000

    Completed exercise h

--- exercise/h_closures_threads/src/main.rs
+++ exercise/h_closures_threads/src/main.rs
@@ -16,8 +16,8 @@ fn expensive_sum(v: Vec<i32>) -> i32 {
     // either need to dereference the argument once in the parameter list like this: `|&x|` or you
     // will need to dereference it each time you use it in the expression like this: `*x`
     v.iter()
-        // .filter() goes here
-        // .map() goes here
+        .filter(|&x| x%2==0 ) // goes here
+        .map(|&x| x*x) // goes here
         .sum()
 }
 
@@ -32,7 +32,9 @@ fn main() {
     // join handle in a variable called `handle`. Once you've done this you should be able to run
     // the code and see the Child thread output in the middle of the main thread's letters
     //
-    //let handle = ...
+    let handle = thread::spawn(move || {
+        expensive_sum(my_vector)
+    });
 
     // While the child thread is running, the main thread will also do some work
     for letter in vec!["a", "b", "c", "d", "e", "f"] {
@@ -44,9 +46,13 @@ fn main() {
     // `handle` variable you stored the join handle in earlier, call .join() to wait for the thread
     // to exit with a `Result<i32, Err>`.  Get the i32 out of the result and store it in a `sum`
     // variable.  Uncomment the println.  If you did 1a and 1b correctly, the sum should be 20.
-    //
-    //let sum =
-    //println!("The child thread's expensive sum is {}", sum);
+
+    // let sum: i32 = match handle.join() {
+    //     Ok(value) => value,
+    //     Err(e) => -1,
+    // };
+    let sum: i32 = handle.join().unwrap();
+    println!("The child thread's expensive sum is {}", sum);
 
     // Time for some fun with threads and channels!  Though there is a primitive type of channel
     // in the std::sync::mpsc module, I recommend always using channels from the crossbeam crate,
@@ -56,14 +62,14 @@ fn main() {
     // flow of execution works.  Once you understand it, alter the values passed to the `pause_ms()`
     // calls so that both the "Thread B" outputs occur before the "Thread A" outputs.
 
-    /*
+    
     let (tx, rx) = channel::unbounded();
     // Cloning a channel makes another variable connected to that end of the channel so that you can
     // send it to another thread.
     let tx2 = tx.clone();
 
     let handle_a = thread::spawn(move || {
-        pause_ms(0);
+        pause_ms(200);
         tx2.send("Thread A: 1").unwrap();
         pause_ms(200);
         tx2.send("Thread A: 2").unwrap();
@@ -74,7 +80,7 @@ fn main() {
     let handle_b = thread::spawn(move || {
         pause_ms(0);
         tx.send("Thread B: 1").unwrap();
-        pause_ms(200);
+        pause_ms(0);
         tx.send("Thread B: 2").unwrap();
     });
 
@@ -89,12 +95,44 @@ fn main() {
     // Join the child threads for good hygiene.
     handle_a.join().unwrap();
     handle_b.join().unwrap();
-    */
 
     // Challenge: Make two child threads and give them each a receiving end to a channel.  From the
     // main thread loop through several values and print each out and then send it to the channel.
     // On the child threads print out the values you receive. Close the sending side in the main
     // thread by calling `drop(tx)` (assuming you named your sender channel variable `tx`).  Join
     // the child threads.
+
+    let (tx3, rx3) = channel::unbounded();
+    let rx4 = rx3.clone();
+
+    let sf_vec = vec![2, 5, 1, 0, 4, 3];
+
+    let handle_c = thread::spawn(move || {
+        for msg in rx3 {
+            println!("Child c thread: Received {}", msg);
+        }
+        println!("Child c done");
+    });
+
+    let handle_d = thread::spawn(move || {
+        for msg in rx4 {
+            println!("Child d thread: Received {}", msg);
+        }
+        println!("Child d done");
+    });
+
+    pause_ms(100);
+    for i in sf_vec {
+        println!("Sending {}", i);
+        tx3.send(i).unwrap();
+    }
+
+    drop(tx3);
+
+    // Join the child threads for good hygiene.
+    handle_c.join().unwrap();
+    handle_d.join().unwrap();
+
+
     println!("Main thread: Exiting.")
 }
